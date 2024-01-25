a328f253d6c101b359163be6596c8bdbd72f1b86
kristian0808 <kristianlleshaj@hotmial.com>
2024-01-25 15:56:51 +0100

    replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -12,11 +12,3 @@ def new_in_list(my_list, idx, element):
     # Return the new list
     return new_list
 
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
