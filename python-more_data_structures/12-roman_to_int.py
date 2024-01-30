ed624ee262f31f9819a8e411607db031ba3ea0fb
kristian0808 <kristianlleshaj@hotmial.com>
2024-01-30 12:43:19 +0100

    Roman to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 
 
 def roman_to_int(roman_string):
+    if isinstance(roman_string, str) is False or roman_string is None:
+        return 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
     res = 0
