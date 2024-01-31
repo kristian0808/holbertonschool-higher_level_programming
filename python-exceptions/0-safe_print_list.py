d9aa1d746d4b57f2cd88bed3b758cb392df79fd7
kristian0808 <kristianlleshaj@hotmial.com>
2024-01-31 09:11:47 +0100

    a function that prints x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(element, end="")
             count += 1
-        except:
+        except IndexError
             break
     print()
     return count
