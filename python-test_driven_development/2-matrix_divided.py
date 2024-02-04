f8018e41cc8b8dbe45204c96a1d1173a041f8e99
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-04 22:54:04 +0100

    func that divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,7 +11,8 @@ def matrix_divided(matrix, div):
     number (div), and returns a new matrix with each element divided by div,
     rounded to 2 decimal places.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or \
+        not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(isinstance(item, (int, float)) for row in matrix for item in row):
