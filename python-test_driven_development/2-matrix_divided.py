b4a2decd0df31f123728c846128f95b15cc46eac
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-04 23:14:25 +0100

    func that divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-
-"""
-This module contains a function that divides all elements of a matrix.
-"""
+""" Substracts all cells of a matrix"""
 
 
 def matrix_divided(matrix, div):
-    """
-    Divides all elements of a matrix.
-    """
-    err1 = "matrix must be a matrix (list of lists) of integers/floats"
-    err2 = "Each row of the matrix must have the same size"
-    err3 = "div must be a number"
-    err4 = "division by zero"
-
-    if type(matrix) is not list or not all(type(row) is list for row in matrix):
-        raise TypeError(err1)
-
-    for row in matrix:
-        if not all(isinstance(item, (int, float)) for item in row):
-            raise TypeError(err1)
-
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError(err2)
-
-    if not isinstance(div, (int, float)):
-        raise TypeError(err3)
-
+    """Takes a matrix and divides every int or float inside it by div"""
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisionError(err4)
+        raise ZeroDivisionError("division by zero")
+    row_len = len(matrix[0])
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        if len(row) != row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+        for elem in row:
+            if type(elem) not in [int, float]:
+                raise_err()
+            new_row.append(round(elem / div, 2))
+        new_matrix.append(new_row)
+    return new_matrix
 
-    return [[round(item / div, 2) for item in row] for row in matrix]
+
+def raise_err():
+    a = "matrix must be a matrix (list of lists) of integers/floats"
+    raise TypeError(a)
