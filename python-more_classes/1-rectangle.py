60a0a89750240dfaf54fa7beb236afd4f5f08217
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-05 14:16:43 +0100

    define rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >=")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
@@ -30,5 +30,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >=")
+            raise ValueError("width must be >= 0")
         self.__width = value
