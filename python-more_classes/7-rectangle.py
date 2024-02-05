1c112ee9baaf660b89c87b57f366d6621df92c57
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-05 22:45:51 +0100

    change representation

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -50,9 +50,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(
-            [str(self.print_symbol) * self.__width for i in range(self.__height)]
-        )
+        return "\n".join([str(self.print_symbol) * self.__width for i in range(self.__height)])
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
