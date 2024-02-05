8d93a5201399aca900fe7f8ee35c779dbe53f6c5
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-05 14:33:57 +0100

    area,perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -40,4 +40,4 @@ class Rectangle:
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.height + self.width)
+        return 2 * (self.__height + self.__width)
