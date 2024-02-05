695b0a8a485c6e01d3e5bcfd7d340a187ad2b55b
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-05 23:27:16 +0100

    class method

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -58,6 +58,10 @@ class Rectangle:
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
 
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
@@ -71,7 +75,3 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         return cls(size, size)
-
-    def __del__(self):
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
