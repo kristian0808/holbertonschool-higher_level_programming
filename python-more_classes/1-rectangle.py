59781053cadf4b5b20f796f8a88cc45d0bd2d666
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-05 14:07:05 +0100

    define rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -33,10 +33,3 @@ class Rectangle:
             raise ValueError("width must be >=")
         self.__height = value
 
-
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
