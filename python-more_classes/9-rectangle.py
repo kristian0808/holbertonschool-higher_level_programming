6bdb8019274403311fc970c4942ce387723268d2
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-06 10:33:44 +0100

    class method

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -19,6 +19,14 @@ class Rectangle:
     def height(self):
         return self.__height
 
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -30,14 +38,6 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     def area(self):
         return self.__height * self.__width
