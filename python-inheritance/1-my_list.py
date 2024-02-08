d500ee4be79ae63a83ee2acaf0e05360b65a782d
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-08 08:07:12 +0100

    ex1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a class MyList that inherits from list:
+"""
+
+
+class MyList(list):
+    """Mylist class"""
+
+    def print_sorted(self):
+        print(sorted(self))
