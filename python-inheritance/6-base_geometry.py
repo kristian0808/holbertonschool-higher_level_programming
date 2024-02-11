40ff7de16d0b126ceb40bd498678c0ead7ef1b5b
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-11 19:45:53 +0100

    ex06

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class BaseGeometry.
+Public instance method: def area(self):
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
