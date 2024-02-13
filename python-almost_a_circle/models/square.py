d0ecc9f00da60f7bfd4f753bb9517064173a6a0d
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-13 20:35:20 +0100

    added dictionary method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,8 +10,8 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}"
-        .format(self.id, self.x, self.y, self.width)
+        return ("[Square] ({}) {}/{} - {}"
+        .format(self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
