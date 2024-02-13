0f0ff16bfd3ebaa5c7b97b2e04dbc1cb34d329f4
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-13 15:17:11 +0100

    setter methods

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,10 +4,18 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ square class """
+    """square class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return ("[Square] ({}) {}/{} - {}"
-                .format(self.id, self.x, self.y, self.width))
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = self.height = value
