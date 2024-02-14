5bbb3c3bb4dc6f58b65a7901a8cb885c1a82b9a6
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-14 15:35:06 +0100

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,7 +4,8 @@ import json
 
 
 class Base:
-    """ base """
+    """base"""
+
     __nb_object = 0
 
     def __init__(self, id=None):
@@ -19,3 +20,9 @@ class Base:
         if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        list_dicts = [obj.to_dictionary() for obj in list_objs] if list_objs else []
+        with open(cls.__name__ + ".json", "w") as file:
+            file.write(cls.to_json_string(list_dicts))
