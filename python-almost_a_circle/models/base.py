96cb656429aa3926df1cba32a4d32246fd95f317
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-15 09:44:38 +0100

    json class method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-""" base class """
+"""base class"""
 import json
 
 
 class Base:
-    """base"""
+    """base class"""
 
-    __nb_object = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            Base.__nb_object += 1
-            self.id = Base.__nb_object
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if not list_dictionaries:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """omeeeeeeeee"""
-        if cls is None:
-            cls = "Rectangle"
         filename = cls.__name__ + ".json"
-        if list_objs is None:
-            list_objs = []
-        serialized_data = [obj.to_dictionary() for obj in list_objs]
-        json_string = cls.to_json_string(serialized_data)
         with open(filename, "w") as file:
-            file.write(json_string)
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(Base.to_json_string(list_dicts))
