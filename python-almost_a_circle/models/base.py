31ef91a40e128aa830b3c011648e146db313941e
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-15 09:19:15 +0100

    json class method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ json """
         if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
