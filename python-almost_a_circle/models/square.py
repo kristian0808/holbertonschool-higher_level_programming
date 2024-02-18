e81607ce5b2a56c355120ea2f589ad93bf601323
kristian0808 <kristianlleshaj@hotmial.com>
2024-02-18 21:57:50 +0100

    update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Write the first class Base"""
+""" class square """
 from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """class Rectangle"""
+    """square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Constructor"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
@@ -16,25 +19,22 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.width = value
-        self.height = value
+        self.width = self.height = value
 
     def update(self, *args, **kwargs):
-        """Update class that assigns an argument"""
-        arg_list = ["id", "size", "x", "y"]
-        for arg_name, arg_value in zip(arg_list, args):
-            setattr(self, arg_name, arg_value)
+        """update method"""
+        nr_args = ["id", "size", "x", "y"]
+        for i in range(len(args)):
+            if i < len(nr_args):
+                setattr(self, nr_args[i], args[i])
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def to_dictionary(self):
         """dictionary"""
-        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
-
-    def __str__(self):
-        """Overwrite str square"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y,
+        }
