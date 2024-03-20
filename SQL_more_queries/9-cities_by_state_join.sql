306871c80aa771f49063352ace5340939a0c388c
kristian0808 <kristianlleshaj@hotmail.com>
2024-03-20 23:42:59 +0100

    cities by states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 --cities by states
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id
+FROM cities INNER JOIN states 
+ON cities.state_id = states.id;
