8eb117f2d94770441d94ffe29edc371be4635aab
kristian0808 <kristianlleshaj@hotmail.com>
2024-03-24 23:05:00 +0100

    all states with orm

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Create engine and establish connection
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query all State objects and sort by states.id
+    states = session.query(State).order_by(State.id).all()
+
+    # Display the results
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    # Close the session
+    session.close()
