fdf39c7107b7d294b545b34e0f819610aeffe6e0
kristian0808 <kristianlleshaj@hotmail.com>
2024-03-24 16:53:33 +0100

    states starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+Write a script that lists all states with a name starting with N
 """
 
 import MySQLdb
