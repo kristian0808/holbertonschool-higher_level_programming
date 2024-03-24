3da3f8f568ac1664fd63648edd7391b97b3797f3
kristian0808 <kristianlleshaj@hotmail.com>
2024-03-24 23:08:46 +0100

    all states with orm

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,13 +5,19 @@ This script lists all State objects from the database hbtn_0e_6_usa.
 
 import sys
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
     # Create engine and establish connection
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            mysql_username, mysql_password, database_name
         ),
         pool_pre_ping=True,
     )
